24848cb1241334eafe455309b79dd91f7e027caf
baminsaring <baminsaring7002@gmail.com>
2026-03-24 16:09:24 +0530

    added toast message and progress bar

--- components/ProgressBar.tsx
+++ components/ProgressBar.tsx
@@ -1,10 +1,15 @@
 import { StyleSheet, Text, View } from "react-native";
 import * as Progress from 'react-native-progress';
 
-export default function CircularProgressBar() {
+type Props = {
+  color: string;
+  size:  number;
+}
+
+export default function CircularProgressBar({ color, size }: Props) {
   return (
     <View>
-      <Progress.Circle size={80} thickness={10} indeterminate={true}/>
+      <Progress.Circle color={color} size={size} thickness={10} indeterminate={true} />
     </View>
   );
 }
